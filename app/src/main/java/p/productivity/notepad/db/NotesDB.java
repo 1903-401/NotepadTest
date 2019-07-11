6569881ea687977e8022b1e37882efd37ea37e9e
k-moma <k-moma@users.noreply.github.com>
2019-07-11 11:34:17 -0400

    try this

---
+++ app/src/main/java/p/productivity/notepad/db/NotesDB.java
@@ -0,0 +1,26 @@
+package p.productivity.notepad.db;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import p.productivity.notepad.model.Note;
+
+@Database(entities = Note.class, version = 1)
+public abstract class NotesDB extends RoomDatabase {
+    public abstract NotesDao notesDao();
+
+    public static final String DATABSE_NAME = "notesDb";
+    private static NotesDB instance;
+
+    public static NotesDB getInstance(Context context) {
+        if (instance == null)
+            instance = Room.databaseBuilder(context, NotesDB.class, DATABSE_NAME)
+                    .allowMainThreadQueries()
+                    .build();
+        return instance;
+    }
+}
